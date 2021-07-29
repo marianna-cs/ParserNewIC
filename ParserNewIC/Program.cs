bab3a3397e13e8470aae5b0ead8c7f1a1fdcd4e6
Marianna <witetigre@gmail.com>
2021-07-29 16:07:22 +0200

    Добавьте файлы проекта.

---
+++ ParserNewIC/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParserNewIC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserAuthorization userAuthorization = new UserAuthorization();
+            userAuthorization.Login();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ParserNewIC/UserAuthorization.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using ParserNewIc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParserNewIC
+{
+    public class UserAuthorization
+    {
+        private IWebDriver _driver = new ChromeDriver();
+
+        private string Email = "Support@bro-car.com.ua";
+
+        private string Password = "153426Aa!";
+
+        private string Linq = "https://sso.intercars.eu/oxauth/login.htm";
+
+        private void LoadLinq()
+        {
+            _driver.Url = Linq;
+            /*WaitHelpers.WaitUntilElementClickable(_driver, By.XPath(@".//button[@type='button']"), 40);
+            var buttonCountry = _driver.FindElement(By.XPath(@".//button[@type='button']"));
+            buttonCountry.Click();*/
+            WaitHelpers.WaitUntilElementClickable(_driver, By.Id(@"loginForm:username"), 40);
+        }
+
+        private void LoginFilling()
+        {
+            var inputUser = _driver.FindElement(By.Id(@"loginForm:username"));
+            inputUser.Clear();
+            inputUser.SendKeys(Email);
+        }
+
+        private void PasswordFilling()
+        {
+            var inputPassword = _driver.FindElement(By.Id(@"loginForm:password"));
+            inputPassword.Clear();
+            inputPassword.SendKeys(Password);
+        }
+
+        private void RememberMe()
+        {
+            var inputRemember = _driver.FindElement(By.Id(@"rememberme"));
+            inputRemember.Click();
+        }
+
+        public void Login()
+        {
+            LoadLinq();
+            LoginFilling();
+            PasswordFilling();
+            RememberMe();
+            var buttonLogin = _driver.FindElement(By.Id(@"loginForm:loginButton"));
+            buttonLogin.Click();
+
+        }
+    }
+}
